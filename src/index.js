0ef89889c502e612b063ae832040f6005e047976
sehdev kumar <55868273+sehdevkumar@users.noreply.github.com>
2021-05-31 17:13:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function handleServer(req, res) {
         res.end();
     }else if (req.url!="/welcome" && req.url==="/contact" ) {}{
         res.writeHead(404);
-        res.write("404"); 
+//         res.write("404"); 
         res.end();
     }
 
