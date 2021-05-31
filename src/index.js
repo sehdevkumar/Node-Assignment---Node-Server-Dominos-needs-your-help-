378082ab6fb9b3ff882dd42d657b8ff73b36c9b0
sehdev kumar <55868273+sehdevkumar@users.noreply.github.com>
2021-05-31 17:03:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 var http = require("http");
 
-const httpServer = http.createServer(handleServer);
+const httpServer = http.createServer(handleServer).listen(8081);
 
 
 function handleServer(req, res) {
-  
+    if (req.url==="/welcome") {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('Welcome to Dominos!'); //write a response to the client
+        res.end();
+    }
+     if (req.url==="/contact") {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const data = JSON.stringify({
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+        });
+        res.write(data); //write a response to the client
+        res.end();
+    }else if (req.url!="/welcome" && req.url==="/contact" ) {}{
+        res.writeHead(404);
+    }
+
+
+
+   
 }
 
 module.exports = httpServer;
